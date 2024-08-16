c80aec5da04c019f0f8fc2a53cc163978c60e465
spatil62 <112513163+spatil62@users.noreply.github.com>
2024-08-16 10:42:41 -0500

    Update spatil62-minio-read---------30.py

--- final/part-two/spatil62-minio-read---------30.py
+++ final/part-two/spatil62-minio-read---------30.py
@@ -10,20 +10,13 @@ import sys
 
 # Required configuration to load S3/Minio access credentials securely - no hardcoding keys into code
 conf = SparkConf()
-conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.3')
-conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
 
-conf.set('spark.hadoop.fs.s3a.access.key', os.getenv('SECRETKEY'))
-conf.set('spark.hadoop.fs.s3a.secret.key', os.getenv('ACCESSKEY'))
-conf.set("spark.hadoop.fs.s3a.endpoint", "http://minio1.service.consul:9000")
-conf.set("fs.s3a.path.style.access", "true")
-conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-conf.set("fs.s3a.connection.ssl.enabled", "false")
+#Removed Some configuration 
 
 # Create SparkSession Object - tell the cluster the FQDN of the host system)
 spark = SparkSession.builder.appName("spatil62-minio-read-part2").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
 
-df = spark.read.csv('s3a://itmd521/30.txt')
+df = spark.read.csv('s3:30.txt')
 
 # Custom code needed to split the original source -- it has a column based delimeter
 # Each record is a giant string - with predefined widths being the columns
@@ -69,52 +62,15 @@ schema_value = StructType([StructField('WeatherStation', StringType(), True),
         StructField('AtmosphericPressure', FloatType(), True),
         StructField('APQualityCode', IntegerType(), True)])
 
-# The coalese() function
-# https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/api/pyspark.sql.DataFrame.coalesce.html
-# This will collapse your DataFrame into a single partition
-# writeDF = splitDF.coalesce(1)
-# splitDF.printSchema()
-
-
-
 
 # Read the partitioned CSV into CSV
-csvdf = spark.read.csv('s3a://itmd521/30.txt', header=True, schema=schema_value)
+csvdf = spark.read.csv('s3:30.txt', header=True, schema=schema_value)
 
 
 
+csvdf.write.format("jdbc").option("url","").option("driver","").option("dbtable","spatil62-thirties").option("user",os.getenv("MYSQLUSER")).option("truncate",True).mode("overwrite").option("password", os.getenv("MYSQLPASS")).save()
 
-#jsondf = spark.read.json('s3a://spatil62/30-newjson')
-#parquetdf = spark.read.parquet('s3a://sn2/30-parquet')
-
-
-
-
-
-
-#datadf.write.mode("overwrite").option("compression", "none").json("s3a://spatil62/30-jsonfile-part2")
-#jsondf = spark.read.json('s3a://spatil62/30-jsonfile-part2')
-
-
-
-
-
-# output DataFrame
-#csvdf.show(10)
-#jsondf.show(10)
-#parquetdf.show(10)
-
-
-
-# Printing schema
-#print(csvdf.schema)
-#print(jsondf.schema)
-#print(parquetdf.schema)
-#sql
-
-csvdf.write.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","spatil62-thirties").option("user",os.getenv("MYSQLUSER")).option("truncate",True).mode("overwrite").option("password", os.getenv("MYSQLPASS")).save()
-
-jdbcDF = csvdf.read.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","spatil62-thirties").option("user",os.getenv("MYSQLUSER")).option("truncate",True).mode("overwrite").option("password", os.getenv("MYSQLPASS")).load()
+jdbcDF = csvdf.read.format("jdbc").option("url","").option("driver","").option("dbtable","spatil62-thirties").option("user",os.getenv("MYSQLUSER")).option("truncate",True).mode("overwrite").option("password", os.getenv("MYSQLPASS")).load()
 
 jdbcDF.show()
 
@@ -122,10 +78,10 @@ print(jdbcDF.schema)
 
 
 
-df.write.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","SN2thirties").option("user",os.getenv('MYSQLUSER')).option("truncate",True).mode("overwrite").option("password",os.getenv('MYSQLPASS')).save()
+df.write.format("jdbc").option("url","").option("driver","").option("dbtable","SN2thirties").option("user",os.getenv('MYSQLUSER')).option("truncate",True).mode("overwrite").option("password",os.getenv('MYSQLPASS')).save()
 
 # connect to the MariaDB server
-Sn2_maria_DB = spark.read.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","SN2thirties").option("user",os.getenv('MYSQLUSER')).option("password", os.getenv('MYSQLPASS')).load()
+Sn2_maria_DB = spark.read.format("jdbc").option("url","").option("driver","").option("dbtable","SN2thirties").option("user",os.getenv('MYSQLUSER')).option("password", os.getenv('MYSQLPASS')).load()
 
 Sn2_maria_DB.show(10)
 Sn2_maria_DB.printSchema()
