823d8f879830d46966445f0db28ffb36062a98f2
spatil62 <112513163+spatil62@users.noreply.github.com>
2024-08-16 10:44:52 -0500

    Update spatil62-minio-read-30.py

--- final/part-two/spatil62-minio-read-30.py
+++ final/part-two/spatil62-minio-read-30.py
@@ -10,20 +10,13 @@ import sys
 
 # Required configuration to load S3/Minio access credentials securely - no hardcoding keys into code
 conf = SparkConf()
-conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.3')
-conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
 
-conf.set('spark.hadoop.fs.s3a.access.key', os.getenv('SECRETKEY'))
-conf.set('spark.hadoop.fs.s3a.secret.key', os.getenv('ACCESSKEY'))
-conf.set("spark.hadoop.fs.s3a.endpoint", "http://minio1.service.consul:9000")
-conf.set("fs.s3a.path.style.access", "true")
-conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-conf.set("fs.s3a.connection.ssl.enabled", "false")
+#removed some configurations 
 
 # Create SparkSession Object - tell the cluster the FQDN of the host system)
-spark = SparkSession.builder.appName("spatil62-minio-read-part2-t").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
+spark = SparkSession.builder.appName("spatil62-minio-read-part2-t").config('').config(conf=conf).getOrCreate()
 
-df = spark.read.csv('s3a://itmd521/30.txt')
+df = spark.read.csv('s3:txt')
 
 # Custom code needed to split the original source -- it has a column based delimeter
 # Each record is a giant string - with predefined widths being the columns
@@ -72,14 +65,14 @@ schema_value = StructType([StructField('WeatherStation', StringType(), True),
 
 
 # Read the partitioned CSV into CSV
-csvdf = spark.read.csv('s3a://spatil62/30-newcsv', header=True, schema=schema_value)
+csvdf = spark.read.csv('s3:30-newcsv', header=True, schema=schema_value)
 
-dataf = spark.read.csv('s3a://spatil62/30-newcsv', header=True, schema=schema_value)
-dataf.write.mode("overwrite").option("compression", "none").json("s3a://spatil62/30-rewriteJSON")
-jsondf = spark.read.json('s3a://spatil62/30-rewriteJSON')
+dataf = spark.read.csv('s3:30-newcsv', header=True, schema=schema_value)
+dataf.write.mode("overwrite").option("compression", "none").json("s3:30-rewriteJSON")
+jsondf = spark.read.json('s3:30-rewriteJSON')
 
-dataf.write.mode("overwrite").option("compression", "none").parquet("s3a://spatil62/30-rewriteParquet")
-parquetdf = spark.read.parquet('s3a://spatil62/30-rewriteParquet')
+dataf.write.mode("overwrite").option("compression", "none").parquet("s3:30-rewriteParquet")
+parquetdf = spark.read.parquet('s3:30-rewriteParquet')
 
 
 # # Display the Schema for the three files. 
@@ -93,9 +86,9 @@ parquetdf.printSchema()
 parquetdf.show(10)
 
 # MARIA DB CONNECTION 
-datadf = spark.read.csv('s3a://itmd521/30.txt', header=True, schema=schema_value)
-datadf.write.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","spatil62Thirties").option("user",os.getenv("MYSQLUSER")).option("truncate",True).mode("overwrite").option("password", os.getenv("MYSQLPASS")).save()
-newDatadF = spark.read.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","spatil62Thirties").option("user",os.getenv("MYSQLUSER")).option("truncate",True).option("password", os.getenv("MYSQLPASS")).load()
+datadf = spark.read.csv('s3:30.txt', header=True, schema=schema_value)
+datadf.write.format("jdbc").option("url","").option("driver","").option("dbtable","spatil62Thirties").option("user",os.getenv("MYSQLUSER")).option("truncate",True).mode("overwrite").option("password", os.getenv("MYSQLPASS")).save()
+newDatadF = spark.read.format("jdbc").option("url","").option("driver","").option("dbtable","spatil62Thirties").option("user",os.getenv("MYSQLUSER")).option("truncate",True).option("password", os.getenv("MYSQLPASS")).load()
 
 newDatadF.show(10)
 newDatadF.printSchema()
