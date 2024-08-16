670b2089ac8d54711524bcee610adb3d39b335f7
spatil62 <112513163+spatil62@users.noreply.github.com>
2024-08-16 10:45:49 -0500

    Update spatil62-minio-part3.py

--- final/part-three/spatil62-minio-part3.py
+++ final/part-three/spatil62-minio-part3.py
@@ -12,15 +12,8 @@ import sys
    
 # Required configuration to load S3/Minio access credentials securely - no hardcoding keys into code
 conf = SparkConf()
-conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.3')
-conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
 
-conf.set('spark.hadoop.fs.s3a.access.key', os.getenv('SECRETKEY'))
-conf.set('spark.hadoop.fs.s3a.secret.key', os.getenv('ACCESSKEY'))
-conf.set("spark.hadoop.fs.s3a.endpoint", "http://minio1.service.consul:9000")
-conf.set("fs.s3a.path.style.access", "true")
-conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-conf.set("fs.s3a.connection.ssl.enabled", "false")
+#removed some configurations 
 
 # Create SparkSession Object - tell the cluster the FQDN of the host system)
 spark = SparkSession.builder.appName("spatil62-minio-part3-second").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
@@ -51,7 +44,7 @@ schema = StructType([StructField('WeatherStation', StringType(), True),
 
 
 # Reading from the parquet
-df = spark.read.parquet('s3a://spatil62/60-parquet',header=True, schema=schema)
+df = spark.read.parquet('s3:60-parquet',header=True, schema=schema)
 print(df.schema)
 df.select("WeatherStation", "VisibilityDistance", "ObservationDate").where(col("VisibilityDistance") < 200).groupBy("WeatherStation", "VisibilityDistance", year("ObservationDate")).count().orderBy(desc(year("ObservationDate"))).show(10)
 
